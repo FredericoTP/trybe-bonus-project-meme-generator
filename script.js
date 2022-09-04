8a9bac88c03f47c663e93b1e3be0563554dc1158
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-04 15:41:42 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const memeImageContainer = document.getElementById('meme-image-container');
 const memeText = document.getElementById('meme-text');
 const memeImage = document.getElementById('meme-image');
 const imagePreview = document.getElementById('image-preview');
+const buttonFire = document.getElementById('fire');
+const buttonWater = document.getElementById('water');
+const buttonEarth = document.getElementById('earth');
 
 function showTextInPreview(event) {
   memeText.innerText = event.target.value;
@@ -19,3 +22,19 @@ function showPreview(event) {
 }
 
 inputImg.addEventListener('change', showPreview);
+
+function changeBorderFire() {
+  memeImageContainer.style.border = '3px dashed rgb(255, 0, 0)';
+}
+
+function changeBorderWater() {
+  memeImageContainer.style.border = '5px double rgb(0, 0, 255)';
+}
+
+function changeBorderEarth() {
+  memeImageContainer.style.border = '6px groove rgb(0, 128, 0)';
+}
+
+buttonFire.addEventListener('click', changeBorderFire);
+buttonWater.addEventListener('click', changeBorderWater);
+buttonEarth.addEventListener('click', changeBorderEarth);
