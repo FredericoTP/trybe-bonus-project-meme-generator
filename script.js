910870dda98d7bd02ee85652cbebce1368522516
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-09-04 15:04:59 -0300

    Requisitos 2, 3 e 4

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const inputText = document.getElementById('text-input');
+const inputImg = document.getElementById('meme-insert');
 const memeImageContainer = document.getElementById('meme-image-container');
 const memeText = document.getElementById('meme-text');
+const memeImage = document.getElementById('meme-image');
+const imagePreview = document.getElementById('image-preview');
 
 function showTextInPreview(event) {
   memeText.innerText = event.target.value;
 }
 
 inputText.addEventListener('keyup', showTextInPreview);
+
+function showPreview(event) {
+  if(event.target.files.length > 0) {
+    let src = URL.createObjectURL(event.target.files[0]);
+    imagePreview.src = src;
+  }
+}
+
+inputImg.addEventListener('change', showPreview);
